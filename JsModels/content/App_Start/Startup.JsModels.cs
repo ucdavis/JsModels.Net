e5f50ed07999069d58cd43361f9b03eee3aa206f
John Knoll <jpknoll@ucdavis.edu>
2015-03-06 11:20:43 -0800

    add content

---
+++ JsModels/content/App_Start/Startup.JsModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JsModels.Owin;
+using Owin;
+
+namespace $rootnamespace$
+{
+    public partial class Startup
+    {
+        public void ConfigureJsModels(IAppBuilder app)
+        {
+            app.MapJsModels(new JsModelsConfiguration()
+            {
+                Types = new[] { typeof(SampleModel), typeof(SampleModel2), typeof(etc) }
+            });
+        }
+    }
+}
