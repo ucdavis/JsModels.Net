331ba711d62f5d20b5c934122fd27a00722342ea
John Knoll <jpknoll@ucdavis.edu>
2015-03-16 12:03:19 -0700

    make required

--- JsModels.Cmd/Options.cs
+++ JsModels.Cmd/Options.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CommandLine;
+﻿using CommandLine;
 using CommandLine.Text;
 
 namespace JsModels.Cmd
 {
     class Options
     {
-        [Option('i', "inputAssembly", DefaultValue = "", HelpText = "Target Input Assebmly")]
+        [Option('i', "inputAssembly", HelpText = "Target Input Assebmly.", Required = true)]
         public string InputAssembly { get; set; }
 
-        [Option('o', "output", DefaultValue = "", HelpText = "Output Directory")]
-        public string OutputDirectory { get; set; }
-        
-        [Option('x', "classes", DefaultValue = "", HelpText = "Comma delimited list of classes to export")]
+        [Option('x', "classes", HelpText = "Comma delimited list of classes to export.", Required = true)]
         public string Classes { get; set; }
+
+        [Option('o', "output", HelpText = "Output Directory.")]
+        public string OutputDirectory { get; set; }
 
         [Option('v', "verbose", DefaultValue = false, HelpText = "Verbose Logging")]
         public bool Verbose { get; set; }
