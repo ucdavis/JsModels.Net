b713870cd4f9f6835a1534143ab667ac33fe7aee
John Knoll <jpknoll@ucdavis.edu>
2015-03-16 10:17:21 -0700

    new cmd proj

---
+++ JsModels.Cmd/Options.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace JsModels.Cmd
+{
+    class Options
+    {
+        [Option("o", "my-option", DefaultValue = 10, HelpText = "This is an option!")]
+        public int MyOption { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this, (current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
---
+++ JsModels.Cmd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsModels.Cmd
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+            }
+        }
+    }
+}
