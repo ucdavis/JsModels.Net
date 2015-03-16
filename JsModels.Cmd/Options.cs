99a61ccf0518d02c4c55cba02c3a4e2f49eb10ed
John Knoll <jpknoll@ucdavis.edu>
2015-03-16 11:39:47 -0700

    Options!

--- JsModels.Cmd/Options.cs
+++ JsModels.Cmd/Options.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,17 @@ namespace JsModels.Cmd
 {
     class Options
     {
-        [Option("o", "my-option", DefaultValue = 10, HelpText = "This is an option!")]
-        public int MyOption { get; set; }
+        [Option('i', "inputAssembly", DefaultValue = "", HelpText = "Target Input Assebmly")]
+        public string InputAssembly { get; set; }
+
+        [Option('o', "output", DefaultValue = "", HelpText = "Output Directory")]
+        public string OutputDirectory { get; set; }
+        
+        [Option('x', "classes", DefaultValue = "", HelpText = "Comma delimited list of classes to export")]
+        public string Classes { get; set; }
+
+        [Option('v', "verbose", DefaultValue = false, HelpText = "Verbose Logging")]
+        public bool Verbose { get; set; }
 
         [HelpOption]
         public string GetUsage()
